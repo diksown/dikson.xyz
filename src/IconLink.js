6aba208244ca18bcb49fcabda7abfb5cadde7f17
Dikson Santos <dikson@mindsight.com.br>
2023-03-26 18:27:14 -0300

    feat: polish css

--- src/IconLink.js
+++ src/IconLink.js
@@ -3,7 +3,7 @@ import Icon from "@mdi/react";
 export default function IconLink({ icon, link }) {
   return (
     <a className="Icon" href={link}>
-      <Icon path={icon} size={1.5} color={"white"} />
+      <Icon path={icon} size={1.5} />
     </a>
   );
 }
